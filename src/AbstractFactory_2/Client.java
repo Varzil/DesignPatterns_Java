f88d4897daf7f6ebe20d04b4564aceebb9925d3b
Varzil <varzilthakkarvvv@gmail.com>
2023-01-18 11:18:20 +0530

    Adding files

--- src/AbstractFactory_2/Client.java
+++ src/AbstractFactory_2/Client.java
@@ -1,5 +1,13 @@
 package AbstractFactory_2;
 
+import java.util.Objects;
+
 public class Client {
+    public static void main(String[] args) {
+        Device dell= Objects.requireNonNull(FactoryGenerator.getFactory(FactoryType.LAPTOPFACTORY)).getGadget(DeviceType.DELl);
+        System.out.println(dell.getDevice());
+        Device iPhone= Objects.requireNonNull(FactoryGenerator.getFactory(FactoryType.MOBILEFACTORY)).getGadget(DeviceType.IPHONE12);
+        System.out.println(iPhone.getDevice());
+    }
 
 }
